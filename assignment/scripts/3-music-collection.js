3aee209070b520179e97abcbb0a7188ab3527d8b
Sarah Helgen <sarah.helgen@gmail.com>
2021-08-09 12:05:24 -0500

    working on showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -23,4 +23,20 @@ console.log( addToCollection( 'Let it Be', 'The Replacements', '1984'));
 console.log( addToCollection( 'Southeastern', 'Jason Isbell', '2013'));
 console.log( addToCollection( '1989', 'Taylor Swift', '2014'));
 console.log(`The albums in the collection array are now ${collection}`);
-console.log( collection );
+console.log('The albums in the collection are now ', collection );
+
+//creating showCollection function that takes in an array parameter and logs each album in the array - sorted TITLE by ARTIST published in YEAR
+function showCollection( array ){
+    console.log( 'in showCollection', array );
+    console.log( 'The number of items in the array is ', array.length );//per instructions, showing the number of items in the array
+    for( let i=0; i<array.length; i++){
+        //looping through each album in the collection to log to console
+        console.log( `The albums in the collection are ${array.title[i]} by ${array.artist[i]} published in ${array.yearPublished[i]}`)
+
+    }//end for loop
+
+}//end showCollection
+
+//calling function to test showCollection function
+console.log( showCollection(collection));
+
